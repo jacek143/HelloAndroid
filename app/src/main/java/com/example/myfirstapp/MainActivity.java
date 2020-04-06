fbbf5df9269e13d193814dcf10f977748b67cf10
Jacek Jankowski <jjankowski@vewd.com>
2020-04-06 15:05:59 +0200

    Start and run service periodically

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.myfirstapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp";
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.step";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +17,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendExpressionForComputation(View view) {
-        String expression = getInputExpression();
-        startActivity(generateIntent(expression));
+    public void startBroadcastingService(View view) {
+        Log.d(this.getClass().getSimpleName(), "Start broadcasting service");
+        Intent intent = new Intent(this, BroadcastingService.class);
+        intent.putExtra(EXTRA_MESSAGE, getStep());
+        startService(intent);
     }
 
-    private Intent generateIntent(String expression) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, expression);
-        return intent;
-    }
-
-    private String getInputExpression() {
+    private String getStep() {
         EditText editText = findViewById(R.id.editText);
         return editText.getText().toString();
     }
